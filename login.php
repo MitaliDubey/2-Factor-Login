94b4ccc8c04100140bf3ba00a7cd4968272fe022
MitaliDubey <78817509+MitaliDubey@users.noreply.github.com>
2022-04-11 14:01:14 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -10,18 +10,18 @@
     require('db.php');
     session_start();
     // When form submitted, check and create user session.
-    if (isset($_POST['username'])) {
-        $username = stripslashes($_REQUEST['username']);    // removes backslashes
-        $username = mysqli_real_escape_string($con, $username);
+    if (isset($_POST['email'])) {
+        $email = stripslashes($_REQUEST['email']);    // removes backslashes
+        $email = mysqli_real_escape_string($con, $email);
         $password = stripslashes($_REQUEST['password']);
         $password = mysqli_real_escape_string($con, $password);
         // Check user is exist in the database
-        $query    = "SELECT * FROM `users` WHERE username='$username'
+        $query    = "SELECT * FROM `users` WHERE email='$email'
                      AND password='" . md5($password) . "'";
         $result = mysqli_query($con, $query) or die(mysql_error());
         $rows = mysqli_num_rows($result);
         if ($rows == 1) {
-            $_SESSION['username'] = $username;
+            $_SESSION['email'] = $email;
             // Redirect to user otp page
             header("Location: verifyotp.php");
         } else {
@@ -34,7 +34,7 @@
 ?>
     <form class="form" method="post" name="login">
         <h1 class="login-title">Login</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
+        <input type="email" class="login-input" name="email" placeholder="email" autofocus="true"/>
         <input type="password" class="login-input" name="password" placeholder="Password"/>
         <input type="submit" value="Login" name="submit" class="login-button"/>
         <p class="link">Don't have an account? <a href="registration.php">Registration Now</a></p>
